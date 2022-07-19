124f312c6b4dca3b3f2f7938be38f221494ccc51
durga9999-dot <durgarao.sirisolla03@gmail.com>
2022-07-19 12:27:46 +0530

    akhi

--- logical.js
+++ logical.js
@@ -1,8 +1,19 @@
 const hasDrivingLicense =true;
 const hasGoodVision=falae;
+const language = true;
 console.log(hasDrivingLicens && hasGoodVision)
 if(hasDrivingLicense && hasGoodVision){
   console.log("durga able to driving")
 }else{
   console.log("durga unable to driving")
+}
+
+const english=true;
+const population =true;
+const isIsland=false;
+const country =true;
+if(language===english && population < 50 && !isIsland){
+  console.log(`sahara live in ${country} :)` )
+}else{
+  console.log(`sahara does n't live in ${country} :)`);
 }
