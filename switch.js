6143e2416fba4bddc17ccf3cd86e70b372147b6d
durga9999-dot <durgarao.sirisolla03@gmail.com>
2022-07-20 00:23:34 +0530

    d

---
+++ switch.js
@@ -0,0 +1,50 @@
+const day ='sunday';
+
+switch(day){
+  case 'monday':
+  console.log('work planning day');
+  break;
+  case 'tuesday':
+  console.log('work implementation day');
+  break;
+  case 'wednesday':
+  console.log('work processing day');
+  break;
+  case 'Thursday':
+  console.log('work recorrection day');
+  break;
+  case 'friday':
+  console.log('work submition day');
+  break;
+  case 'saturday':
+  console.log('relaxing day');
+  break;
+  case'sunday':
+  console.log('entertainement day')
+  break;
+  default:
+  console.log('Invalid day')
+}
+
+
+const language ='chinese';
+
+switch(language){
+  case 'mandarin':
+  console.log('most number of native speakers');
+  break;
+  case 'spanish':
+  console.log('2nd number of native speakers');
+  break;
+  case 'english':
+  console.log('3rd place');
+  break;
+  case 'hindi':
+  console.log('4th place');
+  break;
+  case 'arabic':
+  console.log('most speaken in english');
+  break;
+  default:
+  console.log('Greate language too :D')
+}
