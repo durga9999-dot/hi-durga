470ba851c528e988d7395bd707b7499b393684ec
durga9999-dot <durgarao.sirisolla03@gmail.com>
2022-07-19 11:48:29 +0530

    akhi

---
+++ logical.js
@@ -0,0 +1,8 @@
+const hasDrivingLicense =true;
+const hasGoodVision=falae;
+console.log(hasDrivingLicens && hasGoodVision)
+if(hasDrivingLicense && hasGoodVision){
+  console.log("durga able to driving")
+}else{
+  console.log("durga unable to driving")
+}
