4fe7f1af1d5aa76f4cf11efb433fc1c8cca7e56e
durga9999-dot <durgarao.sirisolla03@gmail.com>
2022-07-27 08:02:20 +0530

    for loop

--- forLoop.js
+++ forLoop.js
@@ -4,3 +4,21 @@ for(let i=0; i<years.length;i++){
   ages.push(2022-years[i])  
 }
 console.log(ages,typeof years)
+
+
+const names=[
+    'firstName',
+    'lastName',
+    'age',
+    'hobbies',
+    'habits'
+    ]
+for(i=names.length-1; i>=0;i--){
+    console.log(names[i])
+}
+for(let durga=1; durga<4; durga++){
+    console.log(`$$$$$$$$this is the ${durga} first loop$$$$$`)
+    for(let bobby=1; bobby<5; bobby++){
+        console.log(`********this the second${bobby} loop******* `)
+    }
+}
